feb5a013240f84ba55f089c494b1153f651df42c
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-02-17 02:40:11 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
-/**
-* main  - A program that prints a line with puts function
-* Return: 0 (Success)
-*/
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-D
-D
 int main()
+{
+    printf("sdeuiw");
+}
+
