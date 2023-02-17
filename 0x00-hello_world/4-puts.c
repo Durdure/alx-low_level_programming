b1bbc0bc889c029ed6a656d799fc73211a3b9843
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-02-17 10:44:38 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 /**
  * main - A program that print a line with puts fnction
  * 
- * return: 0 (succes)
+ * return: 0 (Succes)
 */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle\n");
 
-    return (0);
+    return 0;
 }
 
 
