7a683aebc77eb92f3d704c3873fc995768e880a2
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-02-23 02:46:13 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * _putchar - writes the character C to stdout
- * &C: The character to print
- * Return: On success
- * on error, -1 is returned, and error is 
+ * main - Entry point
+ * Return: Always 0
  */
-int _putchar(char,C)
+
+int main(void)
 {
-	return (write(1, &C, 1));
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchat('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+int _putchar(char C);
+
+void print_alphabet(void);
+
+void print_alphabet_x01(void);
+
+int _islower(int C);
+
+int _isalpha(int C);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void time_table(void);
+
+int add(int, int);
+
+int print_to_98(int n);
+
+void print_times_table(int n);
