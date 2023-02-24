5a20009a4509a7fd74e4dcb499357890da755d2d
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-02-24 03:17:36 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,19 @@
-main.h
-
-int 
+#include "main.h"
+/**
+* main - entry point -> prints _putchar with line feed
+*
+*Return: 0
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
